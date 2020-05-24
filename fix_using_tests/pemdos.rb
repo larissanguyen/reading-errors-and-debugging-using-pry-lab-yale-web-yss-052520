844a2fcdf16cd0c152793144da67b30d0d716072
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-24 05:45:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,12 +3,12 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    # binding.pry
-    # num = 0 
-    # while num < 10
-      string = 10 * "s" + string
-      # num += 1
-    # end
+    binding.pry
+    num = 0 
+    while num < 10
+      string = "s" + string
+      num += 1
+    end
     binding.pry
     puts 'hello'
   else
